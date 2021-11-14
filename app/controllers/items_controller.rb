f1739e665aaf627c814b012526f84f1fd596c835
erin <erinverkoulen93@gmail.com>
2021-11-14 16:48:37 -0600

    commit tree e2e510fd9c2917c7b28ef9b90c0633f6bb80b1f3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,7 @@
 class ItemsController < ApplicationController
+
+    def index 
+        items = Item.all.order(:name)
+        render json: items, include: :user
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.all.find(params[:id])
+        render json: user, include: :items
+
+    end
 end
